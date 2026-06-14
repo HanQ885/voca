1f06114a83823860b269704161f8f17ecbadffcf
HanQ885 <gs25002@gs.hs.kr>
2026-06-15 01:46:19 +0900

    Restrict tasks to matching parts

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-const __vocaAppSource = (window.VOCA_APP_CHUNKS || []).join('');
+let __vocaAppSource = (window.VOCA_APP_CHUNKS || []).join('');
+__vocaAppSource = __vocaAppSource
+  .replace(
+    '    const task2 = DATA.examples.map((item) => {',
+    '    const task2 = DATA.examples.filter((item) => item.part === "Part 2").map((item) => {',
+  )
+  .replace(
+    '    const task3 = DATA.examples.map((item) => {',
+    '    const task3 = DATA.examples.filter((item) => item.part === "Part 3").map((item) => {',
+  )
+  .replace(
+    '    const task4 = DATA.examples.map((item) => {',
+    '    const task4 = DATA.examples.filter((item) => item.part === "Part 4").map((item) => {',
+  );
 window.VOCA_APP_CHUNKS = undefined;
 Promise.resolve(window.VOCA_DATA_READY)
   .then(() => Function(__vocaAppSource)())
